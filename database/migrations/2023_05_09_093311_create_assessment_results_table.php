f6fa4e76220ade77ffb1e3ccffea1c04e8f3682c
tatsumi <cyberhyper1472gmail.com>
2023-05-09 20:58:35 +0630

    code done

--- database/migrations/2023_05_09_093311_create_assessment_results_table.php
+++ database/migrations/2023_05_09_093311_create_assessment_results_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('assessment_results', function (Blueprint $table) {
             $table->id();
+            
             $table->timestamps();
         });
     }
