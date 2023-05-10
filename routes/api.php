3fa44df7f395a0f12e53c72f0889122e4a3e3137
tatsumi <cyberhyper1472gmail.com>
2023-05-10 13:29:27 +0630

    candidate controller

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/auth/register', [AuthController::class, 'createUser']);
-Route::post('/auth/login', [AuthController::class, 'loginUser']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
