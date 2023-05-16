f7093231ad6370332e3ea92028744a541a267506
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2023-05-16 21:26:59 +0630

    conflict resolved

--- app/Models/Candidate.php
+++ app/Models/Candidate.php
@@ -48,8 +48,12 @@ class Candidate extends Model
         return $this->hasMany(SpecificLanguage::class);
     }
 
-    public function assessment()
+    // public function assessment()
+    // {
+    //     return $this->belongsTo(Assessment::class);
+    // }
+    public function assessments()
     {
-        return $this->belongsTo(Assessment::class);
+        return $this->hasMany(Assessment::class);
     }
 }
