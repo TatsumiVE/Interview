d578da466aaf82fb1b6716cd7eadde126e63a51a
Nan May Mi Zhue <maymizhue1679@gmail.com>
2023-05-17 00:08:36 +0630

    conflict done

--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -10,14 +10,8 @@ class UserRepository implements UserRepoInterface
     {
         return User::all();
     }
-<<<<<<< HEAD
-
     public function show($id)
     {
         return User::where('id', $id)->first();
-=======
-    public function show($id){
-       return User::where('id',$id)->first();
->>>>>>> 4a4a9180186570197023fdb91f76aefda92e58df
     }
 }
