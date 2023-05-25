cb1b398a022fd7d8c1bdacbc49d3df81a4c27245
tatsumi <cyberhyper1472gmail.com>
2023-05-25 16:43:46 +0630

    main

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -15,23 +15,19 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     use ApiResponser;
-    //protected $exceptionHandler;
-    // public function __construct(Handler $exceptionHandler)
-    // {
-    //     $this->exceptionHandler = $exceptionHandler;
-    // }
     public function UserLogin(Request $request)
     {
         try {
             $interviewer = Interviewer::where('email', $request->email)->first();
             $interviewerId=$interviewer->id;
-            
+
             if ($interviewer && Auth::attempt(['interviewer_id' => $interviewerId, 'password' => $request->password])) {
                 $user = Auth::user();
                 $success['token'] =  $user->createToken('User API')->plainTextToken;
-                $success['id']=$user->id;
-                $success['name'] =  $user->name;
-
+                $success['id']=$interviewer->id;
+                $success['name'] =  $interviewer->name;
+                $success['role']=$user->getRoleNames();
+                $success['permission']=$user->getPermissionsViaRoles()->pluck("name");
                 return $this->success(200, $success, 'User login successfully.');
             } else {
                 return $this->error(401, ['error' => 'Unauthorized'], 'Unauthorized.');
@@ -39,9 +35,6 @@ class AuthController extends Controller
         } catch (Exception $e) {
 
             return $this->error(500, $e->getMessage(), 'Internal Server Error.');
-
-            // app(Handler::class)->report($e); // Report the exception to the handler
-            // return app(Handler::class)->render($request, $e); // Render the exception response
         }
     }
 }
